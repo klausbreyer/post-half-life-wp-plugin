dacef03328b41d097b46c6f9395abb1329e409a1
Klaus Breyer <kb@buddybrand.de>
2014-07-10 18:37:12 +0200

    removed url

--- phl-admin.php
+++ phl-admin.php
@@ -4,7 +4,8 @@ add_action( 'admin_menu', 'phl_plugin_menu' );
 
 function phl_plugin_menu() {
 	// add_options_page( 'My Plugin Options', 'My Plugin', 'manage_options', 'my-unique-identifier', 'phl_plugin_options' );
-	    add_submenu_page('index.php', 'Post-Half-Life Backend', 'Post-Half-Life (p½)', 'manage_options', 'post-half-life/backend.php', '', plugins_url( 'myplugin/images/icon.png' ), 6 );
+	    add_submenu_page('index.php', 'Post-Half-Life Backend', 'Post-Half-Life (p½)', 'manage_options', 'post-half-life/backend.php', '', '', 6 );
+	    // add_submenu_page('index.php', 'Post-Half-Life Backend', 'Post-Half-Life (p½)', 'manage_options', 'post-half-life/backend.php', '', plugins_url( 'myplugin/images/icon.png' ), 6 );
 }
 
 	
