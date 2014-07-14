1b60e582dfb80cd51dad3c347807445298c31190
Klaus Breyer <kb@buddybrand.de>
2014-07-14 17:30:43 +0200

    .

--- post-half-life.php
+++ post-half-life.php
@@ -1,14 +1,14 @@
 <?php
 /**
 * @package Post_Half_Life
-* @version 0.1
+* @version 0.1.2
 */
 /*
 Plugin Name: Post-Half-Life (p½)
 Plugin URI: http://klaus-breyer.de/projekte/post-half-life
 Description: A Plugin for calculating the half life of a blog post. So you can determine which content and what posting time is best for your content.
 Author: Klaus Breyer
-Version: 0.1
+Version: 0.1.2
 Author URI: http://klaus-breyer.de
 */
 
